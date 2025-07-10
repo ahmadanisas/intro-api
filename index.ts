4bcac3a38dd3daf76cf51fec97830a0749728edd
ahmadanisas <ahmadanisasusatyo@gmail.com>
2025-07-10 11:09:22 +0700

    Delete data

--- index.ts
+++ index.ts
@@ -28,27 +28,28 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/student", (req: Request, res: Response) => {
-  const filters = req.query; // Request input dari user, query string
-  let filteredStudents = dbStudent; // Data semua student
+  // const filters = req.query; // Request input dari user, query string
+  // let filteredStudents = dbStudent; // Data semua student
 
-  // Terapin filternya
-  if (Object.keys(filters).length > 0) {
-    // Cek apakah ada filter yang diterapkan
-    filteredStudents = dbStudent.filter(
-      (
-        student // Cek setiap student yang cocok dengan filter
-      ) =>
-        Object.entries(filters).every(
-          ([key, value]) =>
-            student[key] &&
-            student[key].toString().toLowerCase() ===
-              value!.toString().toLowerCase()
-        )
-    );
-  }
-
-  res.send(filteredStudents); // Server mengembalikan data student jika tidak ada filter
+  // // Terapin filternya
+  // if (Object.keys(filters).length > 0) {
+  //   // Cek apakah ada filter yang diterapkan
+  //   filteredStudents = dbStudent.filter(
+  //     (
+  //       student // Cek setiap student yang cocok dengan filter
+  //     ) =>
+  //       Object.entries(filters).every(
+  //         ([key, value]) =>
+  //           student[key] &&
+  //           student[key].toString().toLowerCase() ===
+  //             value!.toString().toLowerCase()
+  //       )
+  console.log(req.query);
+  res.send(dbStudent);
 });
+
+//   res.send(filteredStudents); // Server mengembalikan data student jika tidak ada filter
+// });
 
 // POST : Untuk membuat dan menambah data baru
 // Note : - Tag url kembar gapapa (/student), tapi request method harus beda
@@ -69,7 +70,7 @@ app.patch("/student/:id", (req: Request, res: Response) => {
   const findIdx = dbStudent.findIndex((val: any) => {
     return val.id === parseInt(req.params.id); // Mencari index dari data yang ingin diubah. pake parseInt karena data dapetnya string, sedangkan id adalah number
   });
-  dbStudent[findIdx] = {...dbStudent[findIdx], ...req.body }; 
+  dbStudent[findIdx] = { ...dbStudent[findIdx], ...req.body };
   // {id: 3, name: "andre", email: "andre@mail.com", name: "Baila", email: "baila@mail.com"}
   // Mengupdate data yang ada di dbStudent
   // Menggunakan spread operator untuk menggabungkan data lama dengan data baru
@@ -83,7 +84,15 @@ app.patch("/student/:id", (req: Request, res: Response) => {
 // DELETE : untuk menghapus data
 app.delete("/student/:id", (req: Request, res: Response) => {
   console.log("DELETE", req.params);
-  res.send("Delete data");
+  const findIdx = dbStudent.findIndex((val: any) => {
+    return val.id === parseInt(req.params.id);
+  });
+dbStudent.splice(findIdx, 1); // Menghapus data yang ada di dbStudent
+  // Pake splice untuk menghapus data dari index yang ditemukan, karena data bisa yg mana aja
+  res.send({
+    message: "Data student berhasil dihapus",
+    result: dbStudent,
+  });
 });
 
 app.listen(PORT, () => {
