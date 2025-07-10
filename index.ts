c37827960595fdb49a5e1164b8206f898aec73d2
ahmadanisas <ahmadanisasusatyo@gmail.com>
2025-07-10 11:38:28 +0700

    api with db.json

--- index.ts
+++ index.ts
@@ -1,19 +1,8 @@
 import express, { Application, Request, Response } from "express";
+import fs from "fs"; // Library untuk mengelola file & directory
+
 
 const PORT: number = 5055; // port number terserah, 4 digit
-
-const dbStudent: any[] = [
-  {
-    id: 1,
-    name: "Hajra",
-    email: "hajra@mail.com",
-  },
-  {
-    id: 2,
-    name: "Arco",
-    email: "arco@mail.com",
-  },
-];
 
 // Define API config
 const app: Application = express();
@@ -44,56 +33,63 @@ app.get("/student", (req: Request, res: Response) => {
   //           student[key].toString().toLowerCase() ===
   //             value!.toString().toLowerCase()
   //       )
-  console.log(req.query);
-  res.send(dbStudent);
+  const data = JSON.parse(fs.readFileSync("./db.json").toString()); // Membaca file db.json
+  res.send(data);
 });
 
 //   res.send(filteredStudents); // Server mengembalikan data student jika tidak ada filter
 // });
 
-// POST : Untuk membuat dan menambah data baru
-// Note : - Tag url kembar gapapa (/student), tapi request method harus beda
-//        - Jika request methodnya sama, maka yang terbaca adalah yang di define duluan (atas)
+// // POST : Untuk membuat dan menambah data baru
+// // Note : - Tag url kembar gapapa (/student), tapi request method harus beda
+// //        - Jika request methodnya sama, maka yang terbaca adalah yang di define duluan (atas)
 app.post("/student", (req: Request, res: Response) => {
-  console.log("POST", req.body);
-  dbStudent.push(req.body); // Menambahkan data baru ke dbStudent
+  // 1. Mengakses data dari file db.json
+  const data = JSON.parse(fs.readFileSync("./db.json").toString()); // Membaca file db.json
+  // 2. Generate id data baru
+  const newId = data(DataTransfer.length-1).id+1; // Mengambil id terakhir dari data yang ada, lalu ditambah 1 untuk id baru
+  // 3. Tambahkan data baru ke array dbStudent
+  data.push({id: newId, ...req.body}); // Menggunakan spread operator untuk menambahkan data baru ke array dbStudent
+  // 4. Tulis kembali data ke file db.json
+  fs.writeFileSync("./db.json", JSON.stringify(data, null, 4)); // Menulis data baru ke file db.json, dengan format JSON yang rapi (4 spasi)
+  // 5. Kirim response ke client
   res.send({
     message: "Data student berhasil ditambahkan",
-    result: dbStudent,
+    result: data,
   });
 });
 
-// PUT / PATCH : untuk memperbarui data. PUT bersifat menimpa data (seluruhnya), sedangkan PATCH bersifat sebagian saja
-// :id adalah parameter dinamis, bisa diisi dengan apa saja
-app.patch("/student/:id", (req: Request, res: Response) => {
-  console.log("PATCH", req.params, req.body); // params: data yang ingin diubah, body: data baru
-  const findIdx = dbStudent.findIndex((val: any) => {
-    return val.id === parseInt(req.params.id); // Mencari index dari data yang ingin diubah. pake parseInt karena data dapetnya string, sedangkan id adalah number
-  });
-  dbStudent[findIdx] = { ...dbStudent[findIdx], ...req.body };
-  // {id: 3, name: "andre", email: "andre@mail.com", name: "Baila", email: "baila@mail.com"}
-  // Mengupdate data yang ada di dbStudent
-  // Menggunakan spread operator untuk menggabungkan data lama dengan data baru
-  // Pastikan value kanan adalah data yang baru, yang kiri data lama
-  res.send({
-    message: "Data student berhasil diperbarui",
-    result: dbStudent[findIdx],
-  });
-});
+// // PUT / PATCH : untuk memperbarui data. PUT bersifat menimpa data (seluruhnya), sedangkan PATCH bersifat sebagian saja
+// // :id adalah parameter dinamis, bisa diisi dengan apa saja
+// app.patch("/student/:id", (req: Request, res: Response) => {
+//   console.log("PATCH", req.params, req.body); // params: data yang ingin diubah, body: data baru
+//   const findIdx = dbStudent.findIndex((val: any) => {
+//     return val.id === parseInt(req.params.id); // Mencari index dari data yang ingin diubah. pake parseInt karena data dapetnya string, sedangkan id adalah number
+//   });
+//   dbStudent[findIdx] = { ...dbStudent[findIdx], ...req.body };
+//   // {id: 3, name: "andre", email: "andre@mail.com", name: "Baila", email: "baila@mail.com"}
+//   // Mengupdate data yang ada di dbStudent
+//   // Menggunakan spread operator untuk menggabungkan data lama dengan data baru
+//   // Pastikan value kanan adalah data yang baru, yang kiri data lama
+//   res.send({
+//     message: "Data student berhasil diperbarui",
+//     result: dbStudent[findIdx],
+//   });
+// });
 
-// DELETE : untuk menghapus data
-app.delete("/student/:id", (req: Request, res: Response) => {
-  console.log("DELETE", req.params);
-  const findIdx = dbStudent.findIndex((val: any) => {
-    return val.id === parseInt(req.params.id);
-  });
-dbStudent.splice(findIdx, 1); // Menghapus data yang ada di dbStudent
-  // Pake splice untuk menghapus data dari index yang ditemukan, karena data bisa yg mana aja
-  res.send({
-    message: "Data student berhasil dihapus",
-    result: dbStudent,
-  });
-});
+// // DELETE : untuk menghapus data
+// app.delete("/student/:id", (req: Request, res: Response) => {
+//   console.log("DELETE", req.params);
+//   const findIdx = dbStudent.findIndex((val: any) => {
+//     return val.id === parseInt(req.params.id);
+//   });
+// dbStudent.splice(findIdx, 1); // Menghapus data yang ada di dbStudent
+//   // Pake splice untuk menghapus data dari index yang ditemukan, karena data bisa yg mana aja
+//   res.send({
+//     message: "Data student berhasil dihapus",
+//     result: dbStudent,
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`API is RUNNING on http://localhost:${PORT}`);
